19450d4a32b91fe953c69965dca11d8aebe06ed5
fidoriel <49869342+fidoriel@users.noreply.github.com>
2024-01-15 00:10:17 +0100

    fix tiny stuff

--- create_datasets_batch_execution_reduce_img_count.py
+++ create_datasets_batch_execution_reduce_img_count.py
@@ -12,7 +12,7 @@ TOTAL_IMAGES = 360
 ORIGINAL_IMAGE_FOLDER = ORIGINAL_DATASET_DIRECTORY/Path("Pepper")
 ORIGINAL_PROJECTIONS_FOLDER = ORIGINAL_IMAGE_FOLDER/Path("projections")
 ORIGINAL_TEMP_FOLDER = ORIGINAL_DATASET_DIRECTORY/Path( "pepper")
-BASE_INI =  Path("/code/root/neat/config/pepper.ini")
+BASE_INI =  Path("/root/neat/configs/pepper.ini")
 
 # executables
 NIKON_TO_NEAT =  Path("/root/neat/build/bin/nikon2neat")
@@ -21,28 +21,30 @@ RECONSTRUCT =  Path("/root/neat/build/bin/reconstruct")
 # relativ config paths
 RELATIVE_TEMP_FOLDER = Path("pepper/")
 RELATIVE_TRAIN = RELATIVE_TEMP_FOLDER/ Path("exp_uniform_25/")
-TRAIN = RELATIVE_TRAIN/ Path("train.txt")
-EVAL = RELATIVE_TRAIN/Path("eval.txt")
+TRAIN =  Path("train.txt")
+EVAL = Path("eval.txt")
 PROJECTIONS = Path("Pepper/projections")
-SCENES = Path("/code/root/neat/scenes")
+SCENES = Path("/root/neat/scenes")
+RELATIVE_CT_CONFIG = Path("Pepper")
 
 def create_xtekct_config(folder: Path, img_count: int) -> None:
-    # load the first ini file which is named *_CT_parameters.xtekct in ORIGINAL_IMAGE_FOLDER
-    ending = "_CT_projections.xtekct"
+    ending = "_CT_parameters.xtekct"
     ini_file = [f for f in os.listdir(ORIGINAL_IMAGE_FOLDER) if f.endswith(ending)][0]
     ini_file = ORIGINAL_IMAGE_FOLDER / ini_file
     config = configparser.ConfigParser()
+    config.optionxform = str
     config.read(ini_file)
 
-    config["XTecCT"]["Projections"] = str(img_count)
-    config["XTecCT"]["AngularStep"] = str(360 / img_count)
+    config["XTekCT"]["Projections"] = str(img_count)
+    config["XTekCT"]["AngularStep"] = str(360 / img_count)
 
     # save the new ini file in the folder
-    with open(folder / f"config{ending}", "w") as configfile:
+    with open(folder /RELATIVE_CT_CONFIG / f"Pepper{ending}", "w") as configfile:
         config.write(configfile)
 
 def create_ini_config(folder: Path, img_count: int) -> None:
     config = configparser.ConfigParser()
+    config.optionxform = str
     config.read(BASE_INI)
 
     with open(folder / f"config.ini", "w") as configfile:
@@ -53,10 +55,11 @@ def create_dataset(folder: Path, img_count: int) -> None:
     projections = folder / PROJECTIONS
     projections.mkdir(parents=True, exist_ok=True)
 
-    image_files = sorted([f for f in os.listdir(input_folder) if f.endswith('.tif')])
+    image_files = sorted([f for f in os.listdir(ORIGINAL_PROJECTIONS_FOLDER) if f.endswith('.tif')])
 
     # take every n image
     every_n = int(len(image_files) / img_count)
+    print(f"every_n: {every_n} out of {len(image_files)} images for {img_count} images")
     tmp = []
     for i in range(0, len(image_files), every_n):
         tmp.append(image_files[i])
@@ -67,21 +70,21 @@ def create_dataset(folder: Path, img_count: int) -> None:
         shutil.copy(ORIGINAL_PROJECTIONS_FOLDER / image_file, projections / image_file)
 
     # create relative temp
-    relative_temp = folder / RELATIVE_TEMP_FOLDER
-    relative_temp.mkdir(parents=True, exist_ok=True)
+    relatiuve_temp = folder / RELATIVE_TRAIN
+    relatiuve_temp.mkdir(parents=True, exist_ok=True)
 
     # eval and train are text files with the image number in each line. they should not overlap. please choose 10% of the images for eval and 10% for train. they shal not overlap
     # create train.txt
     eval_num = 0
-    with open(folder / TRAIN, "w") as f:
+    with open(folder/RELATIVE_TRAIN/ TRAIN, "w") as f:
         for i in range(0, len(image_files), int(len(image_files) / 10)):
             f.write(f"{i}\n")
             eval_num += 1
     
     # create eval.txt
     eval_num = 0
-    with open(folder / EVAL, "w") as f:
-        for i in range(int(len(image_files)), len(image_files), int(len(image_files) / 10)):
+    with open(folder/RELATIVE_TRAIN / EVAL, "w") as f:
+        for i in range(int(int(len(image_files))/20), len(image_files), int(len(image_files) / 10)):
             f.write(f"{i}\n")
             eval_num += 1
     
@@ -91,10 +94,12 @@ def create_dataset(folder: Path, img_count: int) -> None:
 
 
 if __name__ == "__main__":
+    DATASET_OUTPUT_DIRECTORY.mkdir(parents=True, exist_ok=True)
     split_percentages = [1, 0.5, 0.25, 0.125, 0.1, 0.0625]
     datasets = []
     for i in split_percentages:
         input_folder = DATASET_OUTPUT_DIRECTORY / f"pepper_split_{i}"
+        print(f"create dataset with {i * 100}% of the images {input_folder}")
         create_dataset(input_folder, int(TOTAL_IMAGES * i))
         datasets.append(input_folder) 
 
@@ -106,8 +111,8 @@ if __name__ == "__main__":
             else:
                 os.remove(SCENES / f)
         
-        # copy the dataset to SCENES
-        shutil.copytree(dataset, SCENES)
+        # copy the dataset to SCENES rm before
+        shutil.copytree(dataset, SCENES, dirs_exist_ok=True)
 
         # run nikon2neat
         os.system(f"{NIKON_TO_NEAT}")
@@ -123,5 +128,3 @@ if __name__ == "__main__":
             shutil.copytree(experiments / f, NEAT_OUTPUT / f)
         else:
             shutil.copy(experiments / f, NEAT_OUTPUT / f)
-
-
