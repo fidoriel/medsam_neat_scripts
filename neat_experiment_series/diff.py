9cbed58502b0d26ee060f220284c67ebff34e985
luca <luca.kleinschmidt@student.hpi.uni-potsdam.de>
2024-02-17 10:50:55 +0100

    other image diff

--- neat_experiment_series/diff.py
+++ neat_experiment_series/diff.py
@@ -2,13 +2,38 @@ import os
 import argparse
 from wand.image import Image
 import matplotlib.pyplot as plt
+from skimage.measure import compare_ssim
+import cv2
+import numpy as np
+import math
 
-def calculate_diff(base_image_path, compare_image_path):
+def calc_rmsd(base_image_path, compare_image_path):
+    # this calculates the root mean square deviation between two images
     with Image(filename=base_image_path) as base_img:
         with Image(filename=compare_image_path) as comp_img:
             base_img.fuzz = 0.25 * base_img.quantum_range
             _, diff_val = base_img.compare(comp_img, 'root_mean_square')
     return diff_val
+
+def calc_ssim(base_image_path, compare_image_path):
+    # this calculates the structural similarity index between two images
+    base_img = cv2.imread(base_image_path)
+    comp_img = cv2.imread(compare_image_path)
+    gray_base = cv2.cvtColor(base_img, cv2.COLOR_BGR2GRAY)
+    gray_comp = cv2.cvtColor(comp_img, cv2.COLOR_BGR2GRAY)
+    (score, diff) = compare_ssim(gray_base, gray_comp, full=True)
+    return score
+
+def calc_psnr(base_image_path, compare_image_path):
+    # this calculates the peak signal-to-noise ratio between two images
+    base_img = cv2.imread(base_image_path)
+    comp_img = cv2.imread(compare_image_path)
+    mse = np.mean((base_img - comp_img) ** 2)
+    if mse == 0:
+        return 100
+    max_pixel = 255.0
+    psnr = 20 * math.log10(max_pixel / math.sqrt(mse))
+    return psnr
 
 def analyze_experiment_images(directory_path, base_image_path):
     diff_results = {}
@@ -16,9 +41,10 @@ def analyze_experiment_images(directory_path, base_image_path):
         if filename.endswith(".png"):
             experiment_value = int(os.path.splitext(filename)[0])
             compare_image_path = os.path.join(directory_path, filename)
-            diff_val = calculate_diff(base_image_path, compare_image_path)
-            normalized_diff = diff_val / experiment_value
-            diff_results[experiment_value] = normalized_diff
+            diff_val = calc_ssim(base_image_path, compare_image_path)
+            # normalized_diff = diff_val / experiment_value
+            # diff_results[experiment_value] = normalized_diff
+            diff_results[experiment_value] = diff_val
     return diff_results
 
 def plot_graph(diff_results):
